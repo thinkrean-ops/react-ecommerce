4ae004c82eb73e4ac6bd4f3f5c0c616f20205eb4
chhan puthea <thinkrean@gmail.com>
2025-10-01 18:41:23 +0700

    init

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -27,7 +27,7 @@ useEffect (()  => {
 
   return (
     <div className="font-jost">
-      <div  style = {{backgroundImage:"url('./slide_2.jpg')"}}className=" bg-white bg-pink-100 bg-center bg-contain bg-no-repeat    flex items-center h-screen  px-4 lg:px-40">
+      <div  style = {{backgroundImage: "url('./Slide_02.jpg')"}}className=" bg-white bg-pink-100 bg-center bg-contain bg-no-repeat    flex items-center h-screen  px-4 lg:px-40">
 
         <div>
           <h2 className="text-xl font-bold">New Collection</h2>
